d229ba397bda9f0189d28b09d7a90ff41c91ee5e
Michael Durrant <m2@snap2web.com>
2017-10-09 11:16:00 -0400

    Update

--- goto_google.rb
+++ goto_google.rb
@@ -3,10 +3,10 @@ require 'selenium-webdriver'
 require 'capybara'
 
 d=Selenium::WebDriver.for :chrome
-d.navigate.to 'http://google.com'
-e=d.find_element(name: 'q')
-e.send_keys("Hello World of Automation")
-e=d.find_element(name: 'f')
-e.submit
-sleep(3)
+d.navigate.to 'http://salliemae.com'
+#e=d.find_element(name: 'q')
+#e.send_keys("Hello World of Automation")
+#e=d.find_element(name: 'f')
+#e.submit
+sleep(4)
 d.close
