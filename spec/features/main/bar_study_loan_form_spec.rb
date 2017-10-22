56523ef378ffbc49dd7b6d375f5da53e48a9d3e3
Michael Durrant <m2@snap2web.com>
2017-10-22 19:53:57 -0400

    Allow for environments update [skip ci]

--- spec/features/main/bar_study_loan_form_spec.rb
+++ spec/features/main/bar_study_loan_form_spec.rb
@@ -16,19 +16,19 @@ describe 'student loan products' do
 
   def go_to_loan_application_form(page)
     p = page
-    if TEST_ENVIRONMENT == 'production'
-      visit p.bar_study_loan_form_url + p.bar_study_loan_form_id
-    else
-      visit '?NavPoint=APPLY&'+ p.bar_study_loan_form_id
-    end
+    visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
+#    if TEST_ENVIRONMENT == 'production'
+#      visit p.bar_study_loan_form_url + p.bar_study_loan_form_id
+#    else
+#      visit '?NavPoint=APPLY&'+ p.bar_study_loan_form_id
+#    end
     sleep_short
     click_on('Apply for this loan') if TEST_ENVIRONMENT == 'production'
   end
 
   describe "Bar Study Sad Page 1", sad: true, loan_type: 'bar', page_type: 'form' do
     it "has a form for Bar Study student loans that is filled out Incorrectly", happy: true, loan_type: 'graduate' do
-      go_to_loan_application_form(p)
-      click_on('Apply for this loan') if TEST_ENVIRONMENT == 'production'
+      visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
       fill_out_basic_information_form(p,d)
       fill_in p.first_name, with: ''
       continue(p)
@@ -40,12 +40,11 @@ describe 'student loan products' do
 
   describe "Bar Study Happy All Pages", happy: true, smoke: true, loan_type: 'graduate', page_type: 'form' do
     it "has a form for Bar Study student loans", smoke: true do
-      go_to_loan_application_form(p)
-      click_on('Apply for this loan') if TEST_ENVIRONMENT == 'production'
+      visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
       expect(find(p.main_form)).to be
     end
     it "has a form for Bar Study student loans that is filled out correctly", happy: true, loan_type: 'graduate' do
-      go_to_loan_application_form(p)
+      visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
       fill_out_basic_information_form(p,d)
       continue(p)
       sleep_medium # Increased from short to medium to medium_long to pass.  md
