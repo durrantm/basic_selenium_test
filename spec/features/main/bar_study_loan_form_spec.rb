12c6dafb18c1aab6039477a4a06e654f9fa0ade8
Michael Durrant <m2@snap2web.com>
2017-11-13 11:46:44 -0500

    Add nesting (for better failure handling) for bar loan

--- spec/features/main/bar_study_loan_form_spec.rb
+++ spec/features/main/bar_study_loan_form_spec.rb
@@ -7,66 +7,74 @@ describe 'student loan products' do
   p = PageObject.new
   d = FormDataObject.new
 
-  describe "Bar Study Sad Page 1", sad: true, loan_type: 'bar', page_type: 'form' do
-    it "has a form for Bar Study student loans that is filled out Incorrectly", happy: true, loan_type: 'graduate' do
+  describe "Bar Study Form", smoke: true, loan_type: 'bar', page_type: 'form' do
+    it "exists", loan_type: 'graduate' do
       visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
-      fill_out_basic_information_form(p,d)
-      fill_in p.first_name, with: ''
-      continue(p)
-      expect(find_by_id(p.first_name).value).to eq ''
-      expect(find_by_id(p.last_name).value).to eq 'testLast'
-      expect(find_by_id(p.email_address).value).to eq d.email
+        find p.main_form, visible: true, wait: Sleep_lengths[:medium]
+        expect(find(p.main_form)).to be
     end
-  end
 
-  describe "Bar Study Happy All Pages", happy: true, smoke: true, loan_type: 'graduate', page_type: 'form' do
-    it "has a form for Bar Study student loans", smoke: true do
-      visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
-      find p.main_form, visible: true
-      expect(find(p.main_form)).to be
+    describe "Bar Study Sad Page 1", sad: true, loan_type: 'bar', page_type: 'form' do
+      it "has a form for Bar Study student loans that is filled out Incorrectly", happy: true, loan_type: 'graduate' do
+        visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
+        fill_out_basic_information_form(p,d)
+        fill_in p.first_name, with: ''
+        continue(p)
+        expect(find_by_id(p.first_name).value).to eq ''
+        expect(find_by_id(p.last_name).value).to eq 'testLast'
+        expect(find_by_id(p.email_address).value).to eq d.email
+      end
     end
-    it "has a form for Bar Study student loans that is filled out correctly", happy: true, loan_type: 'graduate' do
-      visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
-      fill_out_basic_information_form(p,d)
-      continue(p)
-      fill_out_address(p)
-      continue(p)
-      fill_out_school(p, 'DRAKE')
-      wait_for_ajax
-      find_by_id p.degree, wait:Sleep_lengths[:medium]
-      select 'Juris Doctor (JD)', from: p.degree
-      select 'Full Time', from: p.enrollment_status
-      select 'Jan', from: p.graduation_date_month
-      select this_year, from: p.graduation_date_year
-      select 'Jan', from: p.exam_date_month
-      select this_year, from: p.exam_date_year
-      continue(p)
-      find_by_id p.requested_loan, wait: Sleep_lengths[:medium]
-      fill_in p.requested_loan, with: 10000
-      fill_out_disbursement_information(p, this_year)
-      continue(p)
-      fill_out_employment_information(p)
-      continue(p)
-      fill_out_financial_information(p)
-      continue(p)
-      fill_out_contact_information(p)
-      continue(p)
-      choose_individual_application(p)
-      find p.dialog_frame, wait: Sleep_lengths[:medium]
-      within_frame(find(p.dialog_frame)) do
-        find(p.electronic_consent).click
+
+    describe "Bar Study Happy All Pages", happy: true, smoke: true, loan_type: 'graduate', page_type: 'form' do
+      it "has a form for Bar Study student loans", smoke: true do
+        visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
+        find p.main_form, visible: true
+        expect(find(p.main_form)).to be
       end
-      find(p.dialog_frame)
-      within_frame(find(p.dialog_frame)) do
-        find(p.button_continue).click
+      it "has a form for Bar Study student loans that is filled out correctly", happy: true, loan_type: 'graduate' do
+        visit_url(TEST_ENVIRONMENT, p.bar_study_loan_form_url, p.bar_study_loan_form_id, p)
+        fill_out_basic_information_form(p,d)
+        continue(p)
+        fill_out_address(p)
+        continue(p)
+        fill_out_school(p, 'DRAKE')
+        wait_for_ajax
+        find_by_id p.degree, wait:Sleep_lengths[:medium]
+        select 'Juris Doctor (JD)', from: p.degree
+        select 'Full Time', from: p.enrollment_status
+        select 'Jan', from: p.graduation_date_month
+        select this_year, from: p.graduation_date_year
+        select 'Jan', from: p.exam_date_month
+        select this_year, from: p.exam_date_year
+        continue(p)
+        find_by_id p.requested_loan, wait: Sleep_lengths[:medium]
+        fill_in p.requested_loan, with: 10000
+        fill_out_disbursement_information(p, this_year)
+        continue(p)
+        fill_out_employment_information(p)
+        continue(p)
+        fill_out_financial_information(p)
+        continue(p)
+        fill_out_contact_information(p)
+        continue(p)
+        choose_individual_application(p)
+        find p.dialog_frame, wait: Sleep_lengths[:medium]
+        within_frame(find(p.dialog_frame)) do
+          find(p.electronic_consent).click
+        end
+        find(p.dialog_frame)
+        within_frame(find(p.dialog_frame)) do
+          find(p.button_continue).click
+        end
+        find(p.dialog_frame)
+        within_frame(find(p.dialog_frame)) do
+          find(p.submit_application).click
+        end
+        find(p.title, text: /^Application Status$/, wait: Sleep_lengths[:long])
+        expect(find(p.title, text: /^Application Status$/)).to be
+        sleep_short
       end
-      find(p.dialog_frame)
-      within_frame(find(p.dialog_frame)) do
-        find(p.submit_application).click
-      end
-      find(p.title, text: /^Application Status$/, wait: Sleep_lengths[:long])
-      expect(find(p.title, text: /^Application Status$/)).to be
-      sleep_short
     end
   end
 end
